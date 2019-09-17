80ced45807ea958c4ac8cedcb58e49ec87ac050f
Toukir Ahammed <bsse0806@iit.du.ac.bd>
2019-09-17 15:35:15 +0600

    port changed to 8080

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ app.use(express.static(__dirname + '/views'));
 
 
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8080
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
