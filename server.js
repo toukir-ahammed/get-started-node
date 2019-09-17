e2013b57fc27c7dd4fc43a83450c8dbc030b7de5
Toukir Ahammed <bsse0806@iit.du.ac.bd>
2019-09-17 15:58:07 +0600

    cors enabled

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var cfenv = require("cfenv");
 var bodyParser = require('body-parser')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
