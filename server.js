72111b657fe32a5dd3f58bb3c419874f1d7de297
Toukir Ahammed <bsse0806@iit.du.ac.bd>
2019-11-12 17:09:38 +0600

    Run Public IP

--- server.js
+++ server.js
@@ -378,7 +378,7 @@ app.use(express.static(__dirname + '/views'));
 
 
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
